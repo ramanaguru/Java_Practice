21385adbc81c40e5c930bc2f4616c0fe243419fe
ramanaguru <ramanaguruloyola@gmail.com>
2024-05-15 16:40:10 +0530

    class_Obj commit

---
+++ OOPS/class_Obj.java
@@ -0,0 +1,23 @@
+
+class calculator{
+    public int add(int n1, int n2){
+        int r = n1 + n2;
+        return r;
+    }
+}
+
+
+public class class_Obj{
+    public static void main(String[] args) {
+        int num1= 5;
+        int num2= 5;
+
+        // System.out.println(num1+num2);
+        
+        calculator cal = new calculator(); // object of class calculator
+
+        int result = cal.add(num1, num2);
+        System.out.println(result);
+
+    }
+}
