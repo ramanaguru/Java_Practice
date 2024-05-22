cf5a2cab12bec7b3f8de4d6bca5b73852e9c8412
ramanaguru <ramanaguruloyola@gmail.com>
2024-05-22 12:34:40 +0530

    Initial Commit

---
+++ OOPS/InterfaceExample.java
@@ -0,0 +1,125 @@
+/*
+      What is Interface?
+        - Interface is a reference type in Java.
+        - It is similar to class. It is a collection of abstract methods.
+        - A class implements an interface, thereby inheriting the abstract methods of the interface.
+        
+        Why use Interface?
+        - It is used to achieve total abstraction.
+        - Since Java does not support multiple inheritance in case of class, but by using interface it can achieve multiple inheritance.
+        - It is also used to achieve loose coupling.
+
+        Points to remember:
+
+        Ex: class A extends B implements C, D
+        - A class can extend only one class but can implement multiple interfaces.
+
+        keywords: interface, implements
+        class      ->   class        ===> extends
+        class      -> interface      ===> implements
+        interface  -> interface      ===> extends
+
+
+        - Interface is a blueprint of a class.
+        - It has static constants and abstract methods.
+        - constants are public, static, final by default. so we can't change the value.
+
+
+        Interface vs Abstract class:
+        - Interface can have only abstract methods. Abstract class can have abstract and non-abstract methods.
+        - Interface can have only public static final variables. Abstract class can have final, non-final, static and non-static variables.
+        - Interface can't have a constructor. Abstract class can have a constructor.
+        - Interface can't have instance variables. Abstract class can have instance variables.
+        - Interface can't have a method body. Abstract class can have a method body.
+        - Multiple inheritance is possible in Interface. Multiple inheritance is not possible in Abstract class.
+
+
+  In interview rememeber this point to Explain : 
+      Let's say , You are travelling  and asking  direction to  reach  temple to  person   , he\she will give you direction to reach temple , like go left , right, take 2nd right , you will reach temple.
+      they are giving you direction to reach temple.  but they wont travel/walk  with you , you have to do it by yourself.
+      Same way , Interface is like giving direction to class , what to do , but it wont do it by itself. class has to do it by itself.
+
+
+      Generalising it and not specificing/specializing it.
+
+
+*/
+
+
+
+
+public class InterfaceExample {
+    public static void main(String[] args) {
+        Animal a = new Dog();
+        a.eat();
+
+        Dog d = new Dog();
+        d.eat();
+        d.bark();
+        d.sleep();
+
+        Animal l = new Lion();
+        l.eat();
+        l.sleep();
+
+        Animal t = new Tiger();
+        t.eat();
+        
+        // Cast to Tiger to call run()
+        if (t instanceof Tiger) {   // Check if t is instance of Tiger class or not ,   ==> instancesOf is a keyword in Java which is used to check whether the object is an instance of the specified type (class or subclass or interface).
+          ((Tiger) t).run();
+        }
+    }
+    
+}
+
+interface Animal{
+    public void eat();
+    public void sleep();
+}
+
+abstract class Cat implements Animal{
+
+    public  abstract void bark();
+    
+    public void eat(){
+        System.out.println("Animal is eating");
+    }
+
+    public void sleep(){
+        System.out.println("Animal is sleeping");
+    }
+
+}
+
+class Dog extends Cat{
+    public void bark(){
+        System.out.println("Animal is eating");
+    }
+   
+}
+
+
+class Lion implements Animal{
+    public void eat(){
+        System.out.println("Lion is eating");
+    }
+
+    public void sleep(){
+        System.out.println("Lion is sleeping");
+    }
+}
+
+class Tiger implements Animal{
+    public void eat(){
+        System.out.println("Tiger is eating");
+    }
+
+    public void sleep(){
+        System.out.println("Tiger is sleeping");
+    }
+
+    public void run(){
+      System.out.println("Tiger is running");
+    }
+}
