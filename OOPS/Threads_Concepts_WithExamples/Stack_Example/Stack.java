b652593870b424b8590b34effc769e3faa27410f
ramanaguru <ramanaguruloyola@gmail.com>
2024-05-28 16:14:59 +0530

    Initial Commit

---
+++ OOPS/Threads_Concepts_WithExamples/Stack_Example/Stack.java
@@ -0,0 +1,126 @@
+package Threads_Concepts_WithExamples.Stack_Example;
+
+public class Stack {
+    
+    private int[] array;
+    private int top;
+    Object lock;
+
+    public Stack(int capacity){
+        array = new int[capacity];
+        top = -1;
+        lock = new Object();
+    }
+
+    public boolean isEmpty(){
+        return array.length < 0;
+    }
+
+    public boolean isFull(){
+        return top == array.length - 1;
+    }
+
+    // public boolean push (int element){  =====> If you like to have boolean return type
+    //     synchronized(this){
+    //         if(isFull()){
+    //             return false;
+    //         }
+    
+    //         ++top;
+    
+    //         try{Thread.sleep(1000);}catch(Exception e){ }
+    
+    //         array[top] = element;
+    
+    //         return true;
+
+    //     }
+     
+    // }
+
+    public int push (int element){
+        synchronized(lock){
+            if(isFull()){
+                throw new IllegalStateException("Stack is full");
+            }
+    
+            ++top;
+    
+            try{Thread.sleep(1000);}catch(Exception e){ }
+    
+            array[top] = element;
+    
+            return element;
+
+        }
+     
+    }
+
+
+    public int pop(){
+        synchronized(lock){
+            if(isEmpty()){return Integer.MIN_VALUE;}
+            int element = array[top];
+    
+            array[top] = Integer.MIN_VALUE;
+    
+            try{ Thread.sleep(1000); }catch(Exception e){ }
+    
+            top--;
+    
+            return element;
+        }
+       
+    }
+
+/* This is another way to write the above code
+
+
+    using synchronized keyword in the method signature itself instead of using synchronized block 
+
+    this is will be useful when you want to synchronize the entire method and not just a block of code within the method.
+    this is will lock the object on which the method is called, preventing other threads from accessing the synchronized method on the same object until the lock is released.
+    
+    this is same as using synchronized block with "this" as the lock object.
+
+    
+    public  static  synchronized int push (int element){
+        
+            if(isFull()){
+                throw new IllegalStateException("Stack is full");
+            }
+    
+            ++top;
+    
+            try{Thread.sleep(1000);}catch(Exception e){ }
+    
+            array[top] = element;
+    
+            return element;
+
+        
+     
+    }
+
+
+    public static synchronized int pop(){
+        
+            if(isEmpty()){return Integer.MIN_VALUE;}
+            int element = array[top];
+    
+            array[top] = Integer.MIN_VALUE;
+    
+            try{ Thread.sleep(1000); }catch(Exception e){ }
+    
+            top--;
+    
+            return element; 
+        }
+       
+    }
+
+ */
+
+
+
+}
---
+++ OOPS/Threads_Concepts_WithExamples/Stack_Example/demo.java
@@ -0,0 +1,53 @@
+/*
+ 
+    "synchronized" keyword in Java is used to control access to critical sections of code by multiple threads, 
+    ensuring that only one thread can execute the synchronized method or block at a time. 
+    This is crucial in concurrent programming to prevent race conditions and ensure thread safety when multiple threads are accessing shared resources.
+
+
+    In the context of the Stack class, the synchronized keyword is used to ensure that only one thread can push or pop elements from the stack at a time.
+    This is important because the stack is a shared resource that can be accessed by multiple threads concurrently.
+
+
+
+
+
+    synchronised behind the scenes --> locks the object on which it is called, preventing other threads from accessing the synchronized block or method on the same object until the lock is released.
+    
+
+    
+
+
+
+
+
+*/
+
+
+package Threads_Concepts_WithExamples.Stack_Example;
+
+public class demo {
+    public static void main(String[] args) {
+        Stack stack = new Stack(5);
+        
+        new Thread(() ->{
+            int counter = 0;
+
+            while(++counter <= 5){
+                System.out.println("Pushed: " + stack.push(counter));
+            }
+        }, "PusherThread").start();
+
+
+        new Thread(() -> {
+            int counter = 0;
+
+            while(++counter <= 5){
+                System.out.println("Popped: " + stack.pop());
+            }
+        }, "PopperThread").start();
+        
+    
+
+    }
+}
